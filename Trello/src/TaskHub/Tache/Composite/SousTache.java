23f493ace1cc939ac3f93956a2774d040031b227
Jawad Komodzinski <jawad.komodzinski6@etu.univ-lorraine.fr>
2023-12-13 11:18:41 +0100

    Changement mineur

--- Trello/src/TaskHub/Tache/Composite/SousTache.java
+++ Trello/src/TaskHub/Tache/Composite/SousTache.java
@@ -2,11 +2,18 @@ package TaskHub.Tache.Composite;
 
 public class SousTache extends Tache {
 
+    
     public SousTache(String description) {
         super(description);
     }
 
     @Override
+    /**
+     * Méthods pour ajouter une tâche.
+     * Return false car c'est une sous tâche qui ne peut pas posséder d'autre tâche.
+     * @param st la sous tâche
+     * @return false
+     */
     public boolean ajouterSousTache(Tache st) {
         return false;
     }
