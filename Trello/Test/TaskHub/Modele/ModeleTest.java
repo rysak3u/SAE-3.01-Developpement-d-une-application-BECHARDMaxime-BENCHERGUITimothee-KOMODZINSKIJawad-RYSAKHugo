679116cecfc450a4aa51dbeab00dc9839b7a6efb
Jawad Komodzinski <jawad.komodzinski6@etu.univ-lorraine.fr>
2023-12-14 09:30:55 +0100

    test

---
+++ Trello/Test/TaskHub/Modele/ModeleTest.java
@@ -0,0 +1,28 @@
+package TaskHub.Modele;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ModeleTest {
+
+    @BeforeEach
+    void setUp() {
+        ModeleTache m = new ModeleTache();
+
+    }
+
+    @Test
+    void changerColonneSelectionner() {
+    }
+
+    @Test
+    void creerTache() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
