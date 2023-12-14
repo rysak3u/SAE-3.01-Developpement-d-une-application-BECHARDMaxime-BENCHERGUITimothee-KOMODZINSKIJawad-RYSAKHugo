9fdf19d00c515e49008f71d186332ad9d3f9afd4
unknown <maxime.bechard7@etu.univ-lorraine.fr>
2023-12-14 09:29:46 +0100

    Realignement sur la structure MVC + reorganisation générale + fontionnement de l'appli

--- Trello/src/TaskHub/Tache/Tableau.java
+++ Trello/src/TaskHub/Tache/Tableau.java
@@ -47,45 +47,6 @@ public class Tableau {
         return this.titre;
     }
 
-    /**
-     * méthode pour afficher les tâches en tableau
-     * @return HBox contenant les tâches
-     */
-    public VBox affichage_tableau(){
-        // HBox contenant les tâches
-        HBox hbox = new HBox(50);
-        for(Conteneur colonne : this.colonnes){
-            // on ajoute les tâches de la colonne à la HBox
-            hbox.getChildren().add(colonne.affichage_tableau());
-        }
-        VBox vbox = new VBox();
-        vbox.getChildren().addAll(new Text(this.titre),hbox);
-        // on retourne la VBox
-        return vbox;
-    }
-
-    /**
-     * méthode pour afficher les tâches en liste
-     * @return HBox contenant les tâches
-     */
-    public VBox affichage_liste(){
-
-        // HBox contenant les tâches
-        HBox hbox = new HBox();
-
-        // on parcourt les colonnes
-        for(Conteneur colonne : this.colonnes){
-            // on ajoute les tâches de la colonne à la HBox
-            for(VBox tache : colonne.affichage_liste()){
-                hbox.getChildren().add(tache);
-            }
-        }
-        VBox vbox = new VBox();
-        vbox.getChildren().addAll(new Label(this.titre),hbox);
-        // on retourne la VBox
-        return vbox;
-    }
-
     public Conteneur getConteneur(int i) {
         return this.colonnes.get(i);
     }
@@ -93,4 +54,5 @@ public class Tableau {
     public ArrayList<Conteneur> getConteneurs() {
         return this.colonnes;
     }
+
 }
--- Trello/src/TaskHub/Vue/VueConteneurs.java
+++ Trello/src/TaskHub/Vue/VueConteneurs.java
@@ -1,12 +1,19 @@
 package TaskHub.Vue;
 
+import TaskHub.Controller.ControllerAfficherFormulaire;
 import TaskHub.Modele.ModeleTache;
 import TaskHub.Modele.Sujet;
+import TaskHub.Tache.Composite.Tache;
+import TaskHub.Tache.Conteneur;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
-import javafx.scene.layout.VBox;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 
 public class VueConteneurs extends VBox implements Observateur {
     private ModeleTache modele;
@@ -16,6 +23,38 @@ public class VueConteneurs extends VBox implements Observateur {
     }
     @Override
     public void actualiser(Sujet o) {
-        this.getChildren().setAll(this.modele.getTableau().affichage_tableau());
+        HBox hbox = new HBox(50);
+        hbox.getChildren().setAll();
+        for(Conteneur colonne : this.modele.getTableau().getConteneurs()){
+            // HBox contenant les tâches
+            VBox vbox = new VBox();
+            // Ajout des texts de la HBox
+            vbox.getChildren().add(new Text(colonne.getTitre()));
+            // on ajoute les tâches de la colonne à la VBox
+            for (Tache tache : colonne.getTaches()) {
+                VBox vboxt = new VBox();
+
+                // Ajout des texts de la HBox
+                vboxt.getChildren().addAll(new Text(tache.getTitre()),new Text(tache.getDescription()));
+
+                // Style de la HBox
+                vboxt.setPadding(new Insets(20));
+                vboxt.setPrefSize(200, 100);
+                vboxt.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(3))));
+                // ajout de la tâche dans la liste
+                vbox.getChildren().add(vboxt);
+            }
+            vbox.setAlignment(Pos.TOP_CENTER);
+            Button button = new Button("Ajouter une tâche");
+            button.addEventHandler(MouseEvent.MOUSE_CLICKED,new ControllerAfficherFormulaire(this.modele,this.modele.getTableau().getConteneurs().indexOf(colonne)));
+            button.setPadding(new Insets(10));
+            button.setPrefSize(200, 30);
+            button.setFont(Font.font("Arial Black", FontWeight.BLACK, 15));
+            vbox.getChildren().add(button);
+            vbox.setBorder(new Border(new BorderStroke(Color.BLACK, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, new BorderWidths(3))));
+            hbox.getChildren().add(vbox);
+        }
+        this.getChildren().setAll(new Text(modele.getTableau().getTitre()),hbox);
+
     }
 }
