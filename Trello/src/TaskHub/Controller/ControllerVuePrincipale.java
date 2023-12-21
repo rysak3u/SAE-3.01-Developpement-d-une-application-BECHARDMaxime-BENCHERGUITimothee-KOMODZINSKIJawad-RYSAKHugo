4465738c05e8aba5704305c821d4076b15c2db2f
Jawad Komodzinski <jawad.komodzinski6@etu.univ-lorraine.fr>
2023-12-21 11:44:55 +0100

    Commentaire

--- Trello/src/TaskHub/Controller/ControllerVuePrincipale.java
+++ Trello/src/TaskHub/Controller/ControllerVuePrincipale.java
@@ -5,19 +5,33 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.ComboBox;
 
+/**
+ * Classe ControllerVuePrincipale qui permet de changer la stratégie d'affichage en fonction de la valeur de la ComboBox
+ */
 public class ControllerVuePrincipale implements EventHandler<ActionEvent> {
 
+    // Attribut de la classe
     private VuePrincipal vuePrincipal;
 
+    /**
+     * Constructeur de la classe ControllerVuePrincipale
+     * @param vuePrincipal
+     */
     public ControllerVuePrincipale(VuePrincipal vuePrincipal) {
         this.vuePrincipal = vuePrincipal;
     }
 
+    /**
+     * Méthode qui permet de changer la stratégie d'affichage en fonction de la valeur de la ComboBox
+     * @param event
+     */
     @Override
     public void handle(ActionEvent event) {
+        // On récupère la valeur de la ComboBox
         ComboBox<String> b=(ComboBox<String>)event.getSource();
         String f=b.getValue();
         switch (f){
+            // On change la stratégie d'affichage en fonction de la valeur de la ComboBox
             case "Affichage Bureau":
                 this.vuePrincipal.setAffichage(new VisuelBureau());
                 break;
